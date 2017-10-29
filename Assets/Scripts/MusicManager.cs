f8f40e96cb0f623a2f21164d82d586acaa308db3
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2017-10-29 16:35:18 +0100

    music manager 1

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private int currentSceneIndexNumber;
+
+    // Use this for initialization
+    void Start () {
+        SceneManager.GetActiveScene().buildIndex;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int currentSceneIndexNumber = SceneManager.GetActiveScene();
+
+        if(levelMusicChangeArray[1])
+        {
+            audio.Play();
+        }
+    }
+}
